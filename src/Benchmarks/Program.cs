0f893fb58b9b12a87445a7f858d9eed74622c38a
Folker Kinzel <fkinzel@freenet.de>
2021-12-08 23:53:58 +0100

    Performance tests

--- src/Benchmarks/Program.cs
+++ src/Benchmarks/Program.cs
@@ -1,12 +1,16 @@
-﻿namespace Benchmarks
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace Benchmarks
 {
     class Program
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Nicht verwendete Parameter entfernen", Justification = "<Ausstehend>")]
         static void Main(string[] args)
         {
-
-
+            var summary = BenchmarkRunner.Run<ReplaceLineEndingsBench>();
+            Console.Write("Total Time:");
+            Console.WriteLine(summary.TotalTime);
         }
     }
 }
---
+++ src/Benchmarks/ReplaceLineEndingsBench.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using FolkerKinzel.Strings;
+using FolkerKinzel.Strings.Polyfills;
+
+namespace Benchmarks
+{
+    [MemoryDiagnoser]
+    public class ReplaceLineEndingsBench
+    {
+        public ReplaceLineEndingsBench()
+        {
+            this.TestString = Properties.Resources.ReplaceLineEndingsTest;
+        }
+
+        public string TestString { get; }
+
+        [Benchmark]
+        public StringBuilder BenchStringBuilder1()
+        {
+            var sb = new StringBuilder(TestString);
+            return sb.ReplaceLineEndings("\r\n");
+        }
+
+        [Benchmark]
+        public StringBuilder BenchStringBuilder2()
+        {
+            var sb = new StringBuilder(TestString);
+            return sb.ReplaceLineEndings("\n");
+        }
+
+        [Benchmark]
+        public string BenchString1()
+        {
+            return TestString.ReplaceLineEndings("\r\n");
+        }
+
+        [Benchmark]
+        public string BenchString2()
+        {
+            return TestString.ReplaceLineEndings("\n");
+        }
+    }
+}
