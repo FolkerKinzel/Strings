5d3d6248a2562370e48c462bb2b7ae524a67f1b5
Folker Kinzel <fkinzel@freenet.de>
2023-08-31 00:49:52 +0200

    project reference

--- src/FolkerKinzel.Strings/StringBuilderExtension_AppendBase64.cs
+++ src/FolkerKinzel.Strings/StringBuilderExtension_AppendBase64.cs
@@ -12,12 +12,14 @@ public static partial class StringBuilderExtension
     /// <param name="builder">Der <see cref="StringBuilder"/>, an den Zeichen angefügt werden.</param>
     /// <param name="bytes">Die <see cref="byte"/>-Enumeration, die die anzufügenden Daten enthält.</param>
     /// <returns>Ein Verweis auf <paramref name="builder"/>, nachdem der Anfügevorgang abgeschlossen wurde.</returns>
+    /// <remarks>Die Methode verwendet eine eigene Base64-Implementierung, die etwas langsamer als die BCL-Methoden
+    /// ist, aber für den Zweck sehr viel weniger Heap-Speicher alloziert.</remarks>
     /// <exception cref="ArgumentNullException"><paramref name="builder"/> oder <paramref name="bytes"/> ist <c>null</c>.</exception>
     /// <exception cref="ArgumentOutOfRangeException">Bei der Erhöhung der Kapazität von <paramref name="builder"/>
     /// würde <see cref="StringBuilder.MaxCapacity"/> überschritten.</exception>
     public static StringBuilder AppendBase64(this StringBuilder builder,
-                                                    IEnumerable<byte> bytes,
-                                                    Base64FormattingOptions options = Base64FormattingOptions.None) =>
+                                             IEnumerable<byte> bytes,
+                                             Base64FormattingOptions options = Base64FormattingOptions.None) =>
         bytes is null
                ? throw new ArgumentNullException(nameof(bytes))
                : builder.AppendBase64(CollectionConverter.ToReadOnlySpan(bytes), options);
@@ -30,12 +32,14 @@ public static partial class StringBuilderExtension
     /// <param name="builder">Der <see cref="StringBuilder"/>, an den Zeichen angefügt werden.</param>
     /// <param name="bytes">Das <see cref="byte"/>-Array, das die anzufügenden Daten enthält.</param>
     /// <returns>Ein Verweis auf <paramref name="builder"/>, nachdem der Anfügevorgang abgeschlossen wurde.</returns>
+    /// <remarks>Die Methode verwendet eine eigene Base64-Implementierung, die etwas langsamer als die BCL-Methoden
+    /// ist, aber für den Zweck sehr viel weniger Heap-Speicher alloziert.</remarks>
     /// <exception cref="ArgumentNullException"><paramref name="builder"/> oder <paramref name="bytes"/> ist <c>null</c>.</exception>
     /// <exception cref="ArgumentOutOfRangeException">Bei der Erhöhung der Kapazität von <paramref name="builder"/>
     /// würde <see cref="StringBuilder.MaxCapacity"/> überschritten.</exception>
     public static StringBuilder AppendBase64(this StringBuilder builder,
-                                                    byte[] bytes,
-                                                    Base64FormattingOptions options = Base64FormattingOptions.None) =>
+                                             byte[] bytes,
+                                             Base64FormattingOptions options = Base64FormattingOptions.None) =>
         builder.AppendBase64(bytes is null ? throw new ArgumentNullException(nameof(bytes)) : bytes.AsSpan(), options);
 
 
@@ -46,6 +50,8 @@ public static partial class StringBuilderExtension
     /// <param name="builder">Der <see cref="StringBuilder"/>, an den Zeichen angefügt werden.</param>
     /// <param name="bytes">Die schreibgeschützte Bytespanne, die die anzufügenden Daten enthält.</param>
     /// <returns>Ein Verweis auf <paramref name="builder"/>, nachdem der Anfügevorgang abgeschlossen wurde.</returns>
+    /// <remarks>Die Methode verwendet eine eigene Base64-Implementierung, die etwas langsamer als die BCL-Methoden
+    /// ist, aber für den Zweck sehr viel weniger Heap-Speicher alloziert.</remarks>
     /// <exception cref="ArgumentNullException"><paramref name="builder"/> ist <c>null</c>.</exception>
     /// <exception cref="ArgumentOutOfRangeException">Bei der Erhöhung der Kapazität von <paramref name="builder"/>
     /// würde <see cref="StringBuilder.MaxCapacity"/> überschritten.</exception>
