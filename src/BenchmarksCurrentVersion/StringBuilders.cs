e82631f6611a24b14d56d8e7b524ea2a73a6c587
Folker Kinzel <fkinzel@freenet.de>
2024-05-15 12:30:41 +0200

    benchmarks

--- src/BenchmarksCurrentVersion/StringBuilders.cs
+++ src/BenchmarksCurrentVersion/StringBuilders.cs
@@ -4,6 +4,6 @@ namespace Benchmarks;
 
 static class StringBuilders
 {
-    internal static readonly StringBuilder _50 = new StringBuilder(new string('a', 25)).Append(new string('a', 25));
-    internal static readonly StringBuilder _10 = new StringBuilder(new string('a', 5)).Append(new string('a', 5));
+    internal static readonly StringBuilder _50 = new StringBuilder("a").Append(new string('a', 49));
+    internal static readonly StringBuilder _10 = new StringBuilder("a").Append(new string('a', 9));
 }
