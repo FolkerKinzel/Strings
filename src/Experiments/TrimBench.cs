12833803a54a24a2cd765b496544a5e9f65b1de6
Folker Kinzel <fkinzel@freenet.de>
2024-05-18 12:03:15 +0200

    ReadOnlySpan{char}.Trim(SearchValues{char})

---
+++ src/Experiments/TrimBench.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using FolkerKinzel.Strings;
+
+namespace Experiments;
+
+public class TrimBench
+{
+    private const string TRIM_CHARS = " \"\'";
+    private const string S = "   \"\"\'\' \"";
+
+    private readonly SearchValues<char> _trimChars = SearchValues.Create(TRIM_CHARS);
+
+    [Benchmark]
+    public int TrimEndBcl() => S.AsSpan().TrimEnd(TRIM_CHARS.AsSpan()).Length;
+
+    [Benchmark]
+    public int TrimEndSearchValues() => S.AsSpan().TrimEnd(_trimChars).Length;
+
+    [Benchmark]
+    public int TrimStartBcl() => S.AsSpan().TrimStart(TRIM_CHARS.AsSpan()).Length;
+
+    [Benchmark]
+    public int TrimStartSearchValues() => S.AsSpan().TrimStart(_trimChars).Length;
+
+
+    [Benchmark]
+    public int TrimBcl() => S.AsSpan().Trim(TRIM_CHARS.AsSpan()).Length;
+
+    [Benchmark]
+    public int TrimSearchValues() => S.AsSpan().Trim(_trimChars).Length;
+
+
+
+}
---
+++ src/FolkerKinzel.Strings/ReadOnlySpanExtension_Trim.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolkerKinzel.Strings;
+
+public static partial class ReadOnlySpanExtension
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ReadOnlySpan<char> Trim(this ReadOnlySpan<char> span, SearchValues<char> trimValues)
+        => span.TrimStart(trimValues).TrimEnd(trimValues);
+
+
+    public static ReadOnlySpan<char> TrimStart(this ReadOnlySpan<char> span, SearchValues<char> trimValues)
+    {
+        int idx = span.IndexOfAnyExcept(trimValues);
+        return idx == -1 ? [] : span.Slice(idx);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ReadOnlySpan<char> TrimEnd(this ReadOnlySpan<char> span, SearchValues<char> trimValues)
+        => span.Slice(0, span.LastIndexOfAnyExcept(trimValues) + 1);
+
+
+
+}
+
+
