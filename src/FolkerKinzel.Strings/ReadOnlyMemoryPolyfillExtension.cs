314ae77e586c7fb5e1006faf22acbb96ca8e4c82
Folker Kinzel <fkinzel@freenet.de>
2024-10-03 23:51:09 +0200

    ReadOnlyMemoryPolyfillExtension

--- src/FolkerKinzel.Strings/ReadOnlyMemoryPolyfillExtension.cs
+++ src/FolkerKinzel.Strings/ReadOnlyMemoryPolyfillExtension.cs
@@ -1,6 +1,5 @@
 namespace FolkerKinzel.Strings;
 
-#if NET461 || NETSTANDARD2_0 || NETSTANDARD2_1
 
 /// <summary>Extension methods for the <see cref="ReadOnlyMemory{T}">ReadOnlyMemory&lt;Char&gt;</see>
 /// struct, which are used in .NET Framework 4.6.1, .NET Standard 2.0 and .NET Standard
@@ -14,23 +13,40 @@ public static class ReadOnlyMemoryPolyfillExtension
     /// character memory region.</summary>
     /// <param name="memory">The source memory from which the characters are removed.</param>
     /// <returns>The trimmed character memory region.</returns>
+#if NET461 || NETSTANDARD2_0 || NETSTANDARD2_1
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static ReadOnlyMemory<char> Trim(this ReadOnlyMemory<char> memory)
         => memory.TrimStart().TrimEnd();
+#else
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ReadOnlyMemory<char> Trim(ReadOnlyMemory<char> memory)
+        => MemoryExtensions.Trim(memory);
+#endif
 
     /// <summary>Removes all leading white space characters from a read-only character memory
     /// region.</summary>
     /// <param name="memory">The source memory from which the characters are removed.</param>
     /// <returns>The trimmed character memory region.</returns>
+#if NET461 || NETSTANDARD2_0 || NETSTANDARD2_1
     public static ReadOnlyMemory<char> TrimStart(this ReadOnlyMemory<char> memory)
         => memory.Slice(memory.Length - memory.Span.TrimStart().Length);
+#else
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ReadOnlyMemory<char> TrimStart(ReadOnlyMemory<char> memory)
+        => MemoryExtensions.TrimStart(memory);
+#endif
 
     /// <summary>Removes all trailing white space characters from a read-only character memory
     /// region.</summary>
     /// <param name="memory">The source memory from which the characters are removed.</param>
     /// <returns>The trimmed character memory region.</returns>
+#if NET461 || NETSTANDARD2_0 || NETSTANDARD2_1
     public static ReadOnlyMemory<char> TrimEnd(this ReadOnlyMemory<char> memory)
         => memory.Slice(0, memory.Span.TrimEnd().Length);
+#else
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ReadOnlyMemory<char> TrimEnd(ReadOnlyMemory<char> memory)
+        => MemoryExtensions.TrimEnd(memory);
+#endif
 }
 
-#endif
