273bf63c6598cb5f8f3f164dbb2b306d066ba481
Folker Kinzel <fkinzel@freenet.de>
2024-10-04 18:06:28 +0200

    SpanAction

--- src/FolkerKinzel.Strings/SpanAction.cs
+++ src/FolkerKinzel.Strings/SpanAction.cs
@@ -1,7 +1,5 @@
 namespace FolkerKinzel.Strings;
 
-#if NET461 || NETSTANDARD2_0
-
     /// <summary>Polyfill for the delegate System.Buffers.SpanAction&lt;T,TArg&gt;.</summary>
     /// <typeparam name="T">The type of the objects in the span.</typeparam>
     /// <typeparam name="TArg">The type of object that represents the state. This type parameter
@@ -10,5 +8,3 @@ namespace FolkerKinzel.Strings;
     /// <param name="span">A span of objects of type <typeparamref name="T" />.</param>
     /// <param name="arg">A state object of the type <typeparamref name="TArg" />.</param>
 public delegate void SpanAction<T, in TArg>(Span<T> span, TArg arg);
-
-#endif
